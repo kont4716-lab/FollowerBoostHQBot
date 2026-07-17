1a9e1bff2065e5a69d81cafa39035877aa3bdedb
kont4716-lab <kont4716@gmail.com>
2026-07-17 11:30:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>مرحباً بك</h1>
-    <p>هذا أول موقع لنا.</p>
+    <!DOCTYPE html>
+    <html lang="ar">
+    <head>
+        <meta charset="UTF-8">
+        <title>رفع الصور</title>
+    </head>
+    <body style="font-family:Arial;text-align:center;margin-top:50px;">
+
+        <h1>رفع صورة</h1>
+
+        <form enctype="multipart/form-data">
+            <input type="file" name="image"><br><br>
+
+            <button type="submit">
+                رفع الصورة
+            </button>
+        </form>
+
+    </body>
+    </html>
     """
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=10000
-    )
+    app.run(host="0.0.0.0", port=10000)
